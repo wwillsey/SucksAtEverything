12968bb4a83b0e34680832defa57e97fefd249a2
Jimmy Jin <pengjuj@andrew.cmu.edu>
2015-09-19 16:18:39 -0400

    completed figure 8

--- lab3/odometry_warmup.m
+++ lab3/odometry_warmup.m
@@ -1,27 +1,33 @@
 t = 0;
-k = 2;
 dt = 0.001;
-t_f = 11.207*k;
+ks = 0.5;
+kv = 0.4;
+t_f = 12.565*ks/kv;
 x = 0;
 y = 0;
 theta = 0;
 figure(1);
-axis([-2 2 -2 2]);
-fx = zeros(ceil((t_f-t)/dt));
-fy = zeros(ceil((t_f-t)/dt));
+myPlot = plot(x,y, 'b-');
+xlim([-0.5, 0.5]);
+ylim([-0.5, 0.5]);
+%fx = zeros(1,ceil((t_f-t)/dt));
+%fy = zeros(1,ceil((t_f-t)/dt));
 index = 2;
 while(t < t_f)
-    vr = 1000*(0.1/k + 0.01174*t/k^2);
-    vl = 1000*(0.1/k - 0.01174*t/k^2);
-    w = (vr - vl) / 0.2;
+    vr = 1000*(0.3*kv + 0.14125*kv/ks*sin(t*kv/(2*ks)));
+    vl = 1000*(0.3*kv - 0.14125*kv/ks*sin(t*kv/(2*ks)));
+    w = (vr - vl) / 235;
     V = (vr + vl) / 2;
-    theta = theta + w*dt;
+    theta = theta + w*dt/2;
     x = x + V*cos(theta)*dt;
     y = y + V*sin(theta)*dt;
-    fx(index) = x;
-    fy(index) = y;
+    theta = theta + w*dt/2;
+    set(myPlot, 'xdata', [get(myPlot, 'xdata') x/1000], 'ydata', [get(myPlot, 'ydata'), y/1000]);
+    pause(0.0001);
+    %fx(index) = x;
+    %fy(index) = y;
     index = index+1;
     t = t + dt;
 end
-
-plot(fx, fy);
+axis equal;
+% plot(fx, fy);
