910cb25a039d163d9589dd31fc71f11174fc2c94
William Willsey <wwillsey414@gmail.com>
2015-10-04 16:38:46 -0400

    controller

--- lab5/controller.m
+++ lab5/controller.m
@@ -9,39 +9,15 @@ classdef controller
         function obj = controller()
         end
         
-        function [vl, vr] = getVel(obj, traj, robot, actual_pose)
+        function [V, w] = getVel(obj, traj, actual_pose, dt)
             kpx = .5;
             kpy = .5;
             
             goal_pose = traj.getPoseAtTime(t);
             error = goal_pose - actual_pose;
-            u = 0;
-            t_now = double(handle.LatestMessage.Header.Stamp.Nsec) / 1e9;
-            error_prev = error;
-            if(tp == -1)
-                tp = t_now;
-                error = goal_state - actual_state;
-                error_prev = error;
-            else
-                error = (goal_state - actual_state);
-                max = .25;
+            V = kpx * error(1);
+            w = kpy * error(2);
 
-                %Actual feed back loop
-                ep = error *  kp;
-                ed = ((error - error_prev) / dt) * kd; 
-                ei = (ei + error * dt) * ki;
-                u = ep + ed + ei;
-                if u > max
-                    u = max;
-                elseif u < -max
-                    u = -max;
-                end
-    
-                if  abs(error) <= .0005
-                    u = 0;
-                end
-    
-            end
         end
     end
     
