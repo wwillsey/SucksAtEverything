590bc8bd525ee3b63ed7123bc880aa6221370415
William Willsey <wwillsey414@gmail.com>
2015-10-05 22:57:16 -0400

    added plotter

--- lab5/plotter.m
+++ lab5/plotter.m
@@ -7,6 +7,9 @@ classdef plotter < handle
         all_y;
         all_x1;
         all_y1;
+        all_x2;
+        all_y2;
+        t;
         h;
     end
     
@@ -16,6 +19,9 @@ classdef plotter < handle
             obj.all_y = [];
             obj.all_x1 = [];
             obj.all_y1 = [];
+            obj.all_x2 = [];
+            obj.all_y2 = [];
+            obj.t = [];
             obj.h = num;
         end
         function update_plot(obj, x, y, x1, y1)
@@ -23,10 +29,20 @@ classdef plotter < handle
             obj.all_y = [obj.all_y, y];
             obj.all_x1 = [obj.all_x1, x1];
             obj.all_y1 = [obj.all_y1, y1];
-            figure(obj.h);
-            hold on;
-            plot(obj.all_x, obj.all_y,'r-', obj.all_x1, obj.all_y1, 'b-');
+            %figure(obj.h);
+            set(obj.h, {'XData'}, num2cell([obj.all_x;obj.all_x1],2), {'YData'}, num2cell([obj.all_y, obj.all_y2],2));
+            %drawnow;
+            %plot(obj.all_x, obj.all_y,'r-', obj.all_x1, obj.all_y1, 'b-');
         end 
+        function update_plot_s(obj, x,y,theta, t)
+            obj.all_x = [obj.all_x x];
+            obj.all_x1 = [obj.all_x1 y];
+            obj.all_x2 = [obj.all_x2 theta];
+            obj.t = [obj.t t];
+            
+            set(obj.h, {'YData'}, num2cell([obj.all_x;obj.all_x1;obj.all_x2],2), {'XData'}, num2cell([obj.t; obj.t; obj.t],2));
+            %drawnow;
+        end
      
     end
     
